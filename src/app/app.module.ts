a53a61b144c07da98192e6351f15a8d555fce9ad
momanhaes <momanhaes@gmail.com>
2020-08-07 14:04:05 -0300

    feat: Adiciona componentes home, header, about, restaurants e restaurant. Adiciona também o serviço de restaurante e o app.routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
+import { RouterModule } from "@angular/router";
 
-
-import { AppComponent } from './app.component'
-
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ROUTES } from "./app.routes";
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantsService } from "./restaurants/restaurant/restaurant.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    RouterModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent, RestaurantsComponent, RestaurantComponent],
+  imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
+  providers: [RestaurantsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+import { Restaurant } from "./restaurant/restaurants.model";
+import { RestaurantsService } from "./restaurant/restaurant.service";
+
+@Component({
+  selector: "mt-restaurants",
+  templateUrl: "./restaurants.component.html",
+})
+export class RestaurantsComponent implements OnInit {
+  restaurants: Restaurant[];
+
+  constructor(private restaurantService: RestaurantsService) {}
+
+  ngOnInit() {
+    this.restaurants = this.restaurantService.restaurants();
+  }
+}
