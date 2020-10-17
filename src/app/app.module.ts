6d884fd6f27db8e6182ffec2e0239ad3e626a475
momanhaes <momanhaes@gmail.com>
2020-10-17 06:03:17 -0300

    feat: Criação do componente de compra.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
 import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
+    OrderComponent,
   ],
   imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
   providers: [
