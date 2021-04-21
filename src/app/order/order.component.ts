af1202fb7009169fc16ac8c5fac297a72984d7da
Matheus Manhães <matheus.manhaes@ed.company>
2021-04-21 15:59:34 -0300

    feat: Insere chama de efetivação de compra e componente de sucesso

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { RadioOption } from "app/shared/radio/radio-option.model";
+import { Order, OrderItem } from "./order.model";
 import { OrderService } from "./order.service";
 
 @Component({
@@ -26,12 +28,12 @@ export class OrderComponent implements OnInit {
     },
   ];
 
-  constructor(private orderService: OrderService) {}
+  constructor(private orderService: OrderService, private router: Router) {}
 
   ngOnInit() {}
 
   itemsValue(): number {
-    return this.orderService.itemsValue()
+    return this.orderService.itemsValue();
   }
 
   cartItems(): CartItem[] {
@@ -48,5 +50,19 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem) {
     this.orderService.remove(item);
+  }
+
+  checkOrder(order: Order, validated: boolean) {
+    if (!validated) {
+      return;
+    }
+
+    order.orderItems = this.cartItems().map(
+      (item: CartItem) => new OrderItem(item.quantity, item.menuItem.id)
+    );
+    this.orderService.checkOrder(order).subscribe((orderId: string) => {
+      this.router.navigate(["/order-summary"]);
+      this.orderService.clear();
+    });
   }
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from "@angular/core";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { MEAT_API } from "app/app.api";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { Observable } from "rxjs/Observable";
+import { Order, OrderItem } from "./order.model";
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class OrderService {
-  constructor(private cartService: ShoppingCartService) {}
+  constructor(private cartService: ShoppingCartService, private http: Http) {}
 
   cartItems(): CartItem[] {
     return this.cartService.items;
@@ -24,5 +29,23 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.cartService.removeItem(item);
+  }
+
+  clear() {
+    this.cartService.clear();
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers();
+    headers.append("Content-Type", "aplication/json");
+
+    return this.http
+      .post(
+        `${MEAT_API}/orders`,
+        JSON.stringify(order),
+        new RequestOptions({ headers: headers })
+      )
+      .map((response) => response.json())
+      .map((order) => order.id);
   }
 }
