0b8ca5ed9f4d249e3af5763d5a7fa20bf4ba98ee
Matheus Manhães <matheus.manhaes@ed.company>
2021-04-22 19:13:17 -0300

    feat: Separa a aplicação por módulo e aplica estratégia de lazy loading.

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from "@angular/common";
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { OrderService } from "app/order/order.service";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurant/restaurant.service";
+import { InputComponent } from "./input/input.component";
+import { RadioComponent } from "./radio/radio.component";
+import { RatingComponent } from "./rating/rating.component";
+
+@NgModule({
+  declarations: [InputComponent, RadioComponent, RatingComponent],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  exports: [
+    InputComponent,
+    RadioComponent,
+    RatingComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
+})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [ShoppingCartService, RestaurantsService, OrderService],
+    };
+  }
+}
