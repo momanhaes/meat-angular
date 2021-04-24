b643c0b271d0a854712c3d54d706663eee9e3487
Matheus Manhães <matheus.manhaes@ed.company>
2021-04-24 17:14:13 -0300

    feat: Insere animações para carrinho de compras, restaurante e item do menu

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { MenuItem } from "./menu-item.model";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
 
 @Component({
   selector: "mt-menu-item",
   templateUrl: "./menu-item.component.html",
+  animations: [
+    trigger("menuItemAppeared", [
+      state(
+        "ready",
+        style({
+          opacity: 1,
+        })
+      ),
+      transition("void => ready", [
+        style({ opacity: 0, transform: "translateX(-20px)" }),
+        animate("300ms 0s ease-in"),
+      ]),
+    ]),
+  ],
 })
 export class MenuItemComponent implements OnInit {
   @Input() menuItem: MenuItem;
   @Output() add = new EventEmitter();
+  public menuItemState = "ready";
 
   constructor() {}
 
