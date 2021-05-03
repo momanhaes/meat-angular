9e918c00bd75d4a3e7b4e676dbbc0ffed68d46ca
Matheus Manhães <matheus.manhaes@ed.company>
2021-05-03 15:03:06 -0300

    refactor: Refatora error handler global considerando a classe ErrorHandler e zonas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, LOCALE_ID } from "@angular/core";
+import { NgModule, LOCALE_ID, ErrorHandler } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { PreloadAllModules, RouterModule } from "@angular/router";
 
@@ -18,8 +18,9 @@ import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 import { SharedModule } from "./shared/shared.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NotFoundComponent } from "./not-found/not-found.component";
-import { LoginComponent } from './security/login/login.component';
-import { UserDetailComponent } from './header/user-detail/user-detail.component';
+import { LoginComponent } from "./security/login/login.component";
+import { UserDetailComponent } from "./header/user-detail/user-detail.component";
+import { ApplicationErrorHandler } from "./app.error.handler";
 
 @NgModule({
   declarations: [
@@ -45,7 +46,10 @@ import { UserDetailComponent } from './header/user-detail/user-detail.component'
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules }),
   ],
-  providers: [{ provide: LOCALE_ID, useValue: "pt-BR" }],
+  providers: [
+    { provide: LOCALE_ID, useValue: "pt-BR" },
+    { provide: ErrorHandler, useClass: ApplicationErrorHandler },
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,8 +4,6 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/catch";
 
 @Injectable()
 export class RestaurantsService {
